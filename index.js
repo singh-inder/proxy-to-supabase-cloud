d076a95623146b78f47848d4d9cbfe8d5b3d3b8f
Muhammed <mail.muhammed2002@gmail.com>
2025-09-16 23:13:26 +0400

    proxy status check endpoint to ping using cron job

--- index.js
+++ index.js
@@ -24,6 +24,13 @@ for (const path of paths) {
   });
 }
 
+// This route is used by an external cron job to keep the server instance alive on hosting platforms like Render
+// Free tier may goes to sleep after 15 min of inactivity
+
+server.get("/proxy-status", async (request, reply) => {
+  return { status: "ok", service: "supabase-proxy" };
+});
+
 // PORT will be provided by render
 server
   .listen({ port: process.env.PORT || 3000, host: "0.0.0.0" })
